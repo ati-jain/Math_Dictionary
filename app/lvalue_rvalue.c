74ad970e608034c47b8aa05f8c9fb4b50f934029
Atishay Jain <84273129+ati-jain@users.noreply.github.com>
2022-04-15 15:49:11 +0530

    Delete app directory

--- app/lvalue_rvalue.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    int i = 4;
-    (++i)++;
-    return 0;
-}
-//In C, prefix and postfix operators need integer-value to perform an operation and return r-value. The expression (++i)++ when executed increments the value of variable i(i is a integer-value) and returns r-value. The compiler generates the error(integer-value required) when it tries to post-increment the value of a r-value.
